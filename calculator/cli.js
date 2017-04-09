52ffdbc2654f8f705ddad75b97980f4c60f2c4dd
Dipesh <dipesh.290193@gmail.com>
2017-04-08 19:59:41 -0700

    calculator using node js, inputs from terminal

---
+++ calculator/cli.js
@@ -0,0 +1,19 @@
+const 
+	yargs = require('yargs')
+	quiz = require('./app')
+
+const flags = yargs.usage('$0: usage --run')
+	.options('h',{
+		alias: 'help',
+		describe: 'display help'
+	})
+	.options('r',{
+		alias: 'run',
+		describe: 'run the quiz'
+	})
+	.argv
+
+if(flags.help)
+	yargs.showHelp()
+if(flags.run)
+	quiz.run()
